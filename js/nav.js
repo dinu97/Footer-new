f55850eebd898cbbaf543d412faacb0f0cf2a5eb
dinu97 <44730952+dinu97@users.noreply.github.com>
2022-11-04 16:58:33 +0530

    changes apply

--- js/nav.js
+++ js/nav.js
@@ -1,54 +1,48 @@
-
-// var addbtns = document.getElementsById("addNewElement-btn-two");
 var closebtns = document.getElementsByClassName("close");
-var modal = document.getElementById('headerComponentBox-Id');
-var modal2 = document.getElementById('ImageUpload-Id');
-
+var modal = document.getElementById("headerComponentBox-Id");
+var modal2 = document.getElementById("ImageUpload-Id");
 
 var i;
 
 for (i = 0; i < closebtns.length; i++) {
-    closebtns[i].addEventListener("click", function () {
-        this.parentElement.remove();
-    });
+  closebtns[i].addEventListener("click", function () {
+    this.parentElement.remove();
+  });
 }
 
 
 
-// for (j = 0; j < addbtns.length; j++) {
-//     addbtns[j].addEventListener("click", function () {
-//         this.parentElement.();
-//     });
-// }
+let BtnAddElement = document.querySelector(".btnNav-add");
+let DivContainer = document.getElementById("navbar-navId");
 
 
-function myFunction() {
+BtnAddElement.addEventListener("click", AddNewEliment);
 
-    // Create an "li" node:
-    const node = document.createElement("li");
-    
-    // Create a text node:
-    const textnode = document.getElementById("addNewElement-btn-two");
-    
-    // Append the text node to the "li" node:
-    node.appendChild(textnode);
-    
-    // Append the "li" node to the list:
-    document.getElementById("navbar-navId").appendChild(node);
-    }
-
-
-function showFunction(){
-    modal.style.display = "block";
-  }
+function AddNewEliment(){
+  let newDiv = document.createElement("div");
+  let r = document.createTextNode("NAV BUTTON");
+  console.log("add");
+  newDiv.classList.add('add-newElement');
   
-  function closeFunction(){
-    modal.style.display = "none";
-  }
+  
 
-function showFunction1(){
-    modal2.style.display = "block";
+  DivContainer.appendChild(newDiv).appendChild(r);
+
 }
-function closeFunction1(){
-    modal2.style.display = "none";
-  }
+
+
+
+function showFunction() {
+  modal.style.display = "block";
+}
+
+function closeFunction() {
+  modal.style.display = "none";
+}
+
+function showFunction1() {
+  modal2.style.display = "block";
+}
+function closeFunction1() {
+  modal2.style.display = "none";
+}
